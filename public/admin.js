3e75c910f52823520a92a547e05121b0a10146ca
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-31 22:47:21 -0700

    added displayBook(book) to display a list of book titles and quantities to the admin with functionality to update book quantities

--- public/admin.js
+++ public/admin.js
@@ -1,7 +1,36 @@
-async function main() {
+async function main(){
     let res = await fetch('http://localhost:3001/listBooks')
 
-    let books = res.json()
+    let books = await res.json()
+
+    books.forEach(displayBook)
+}
+
+function displayBook(book){
+    let rootDiv = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let input = document.createElement('input')
+    input.value = book.quantity
+
+    let saveButt = document.createElement('button')
+    saveButt.textContent = 'Save'
+
+    saveButt.addEventListener('click', function(){
+        fetch('http://localhost:3001/updateBook',{
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ 
+                id: book.id,
+                quantity: input.value
+            })
+        })
+    })
+
+    li.append(input, saveButt)
+    rootDiv.append(li)
 }
 
 main()
